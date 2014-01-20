b52daa259c44ece0877743ab3f9ff10598cdbe34
Isaac Cohen <icohen@leapmotion.com>
2014-01-19 16:16:29 -0800

    getting started on talk

---
+++ html5_webGL.js
@@ -0,0 +1,107 @@
+define(function(require, exports, module) {
+
+  var Womb                = require( 'app/Womb'                       );
+
+  var Ring                = require( 'app/scenes/html5_webGL/Ring'    );
+  var Text                = require( 'app/scenes/html5_webGL/Text'    );
+
+
+  /*
+   
+     Create our womb
+
+  */
+  var link = 'http://soundcloud.com/';
+  var info =  "Drag to spin, scroll to zoom,<br/> press 'x' to hide interface";
+  
+  womb = new Womb({
+    cameraController: 'TrackballControls',
+    modelLoader:      true,
+    textCreator:      true,
+    raycaster:        true,
+    title:            'Turbulence: Finding and Making your Happy Place',
+    link:             link, 
+    summary:          info,
+    gui:              true,
+    imageLoader:      true,
+    stats:            true,
+    color:            '#000000',
+    failureVideo:     84019684,
+    test: true,
+    size:             400
+  });
+
+  womb.stream = womb.audioController.createUserAudio();
+
+  womb.modelLoader.loadFile( 'OBJ' , '/lib/models/mug_11530_10.obj' , function( object ){
+
+      if( object[0] instanceof THREE.Geometry ){
+
+        womb.geo = object[0];
+        womb.geo.computeFaceNormals();
+        womb.geo.computeVertexNormals();
+        
+        womb.modelLoader.assignUVs( womb.geo );
+       
+        womb.onMugLoad( womb.geo );
+      }
+
+  });
+
+  womb.onMugLoad = function( geo ){
+
+   /* womb.voicePulser1  = new Ring( womb , {
+      radius: womb.size / 5,
+      geo:geo,    
+      color: new THREE.Vector3( .9 , .4 , .0 ),
+      numOf: 30
+    });*/
+
+   // womb.
+
+  }
+
+  console.log( womb.size );
+  womb.voicePulser1  = new Ring( womb , {
+    radius: womb.size / 5,
+    //geo:geo,    
+    color: new THREE.Vector3( .9 , .4 , .0 ),
+    numOf: 1,
+    opacity: 1
+
+  });
+
+  womb.happy = new Text( womb , {
+
+    text: 'HAPPY!',
+    color: new THREE.Vector3( 1.5 , 1.5 , 1.5 ),
+
+  });
+
+  womb.happy.enter();
+
+  womb.stream.onStreamCreated =  function(){
+  
+  
+  }
+
+  womb.loader.loadBarAdd();
+  
+  womb.update = function(){
+
+  }
+
+  womb.start = function(){
+
+  }
+
+  womb.raycaster.onMeshHoveredOver = function(){
+
+  }
+
+  womb.raycaster.onMeshHoveredOut = function(){
+
+  }
+  
+
+});
--- js/app/three/FBOSimulation.js
+++ js/app/three/FBOSimulation.js
@@ -19,12 +19,15 @@ define(function(require, exports, module) {
 
   function FBOSimulation( womb , params ){
 
+    this.womb = womb;
 
     this.params = _.defaults( params || {} , {
 
-      size: 
-      positionShader:
-      velocityShader:
+      size:
+
+      // Should always be the same type of shader
+      physicsVertShader:
+      physicsFragShader:
 
       // must include map?
       particlesUniforms:
@@ -37,12 +40,59 @@ define(function(require, exports, module) {
 
     });
 
+    this.renderer = this.womb.renderer;
+
+     
+    this.size = this.params.size;
+
+
+    this.positionsTexture = this.createDataTexture( this.size );
+
+    this.physicsShader = new THREE.ShaderMaterial({
+
+      uniforms        : this.params.physicsUniforms,
+      vertexShader    : this.params.physicsVertShader,
+      fragmentShader  : this.params.physicsFragShader
+
+    });
+
+    this.positionTexture = this.createDataTexture( this.size );
+
+    this.rtTexturePos = this.createRenderTexture( size );
+
+
     this.fbo = fboUtils( this.size , this.renderer , this.physicsShader ); 
 
 
   }
 
-  FBOSimulation.prototype
+  FBOSimulation.prototype.createDataTexture( size ){
+
+    var data = new Float32Array( size * size * 4 );
+    var texture = new THREE.DataTexture( data, size , size , THREE.RGBAFormat, THREE.FloatType );
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+    texture.generateMipmaps = false;
+    texture.needsUpdate = true;
+
+    return texture;
+
+  }
+
+  FBOSimulation.protoype.createRenderTexture( size ){
+
+    var texture = new THREE.WebGLRenderTarget( size , size , {
+      minFilter: THREE.NearestFilter,
+      magFilter: THREE.NearestFilter,
+      format: THREE.RGBAFormat,
+      type:THREE.FloatType,
+      stencilBuffer: false
+    });
+
+
+    return texture;
+
+  }
 
 
   module.exports = FBOSimulation;
